363f6054900c705f2cbe58908420536bdf8b6573
Ilya Wise <ilyaphotoshop17@gmail.com>
2019-11-28 21:30:10 +0200

    [add] added a reminder

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 
-const telegramLogo = document.querySelector('#telegramLogo');
-const gmailLogo = document.querySelector('#gmailLogo');
-const instagramLogo = document.querySelector('#instagramLogo');
+const telegramLogo = document.querySelector('#telegramLogo').addEventListener('click', () => {
+    alert('Link Telegram')
+});
+const gmailLogo = document.querySelector('#gmailLogo').addEventListener('click', () => {
+    alert('Link Gmail')
+});;
+const instagramLogo = document.querySelector('#instagramLogo').addEventListener('click', () => {
+    alert('Link Instagram')
+});;
 
 
